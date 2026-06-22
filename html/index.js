1df9712b067652acb10e3393a00917945abf89d6
dhoijsl5 <juanda170608@gmail.com>
2026-06-22 17:01:34 +0200

    subiendo todos los archivos del proyecto

---
+++ html/index.js
@@ -0,0 +1,33 @@
+
+let edad = 18;
+var niña= true;
+const nombre= "rafael"
+nombre="sierra"
+
+console.log(niña)
+let edad = 18;
+edad=26;
+var niña= true;
+const nombre= "rafael"
+if(true){
+    let edad=18
+}
+if(true){
+    var cabello=azul;
+}
+
+saludar()
+
+function saludar (){
+    let nombre="rafael";
+    var edad = 18;
+    const ojos = 2
+}
+
+ var minombre;
+ if(true) {
+    var minombre = 'rafael';
+    let color = 'azul';
+ }
+ console.log(minombre);
+ console.log(color);
